b9e8d3d0c2a77b1d1cbf8ebe8c0e508c801dae3f
chingya1214 <adt105115@gm.ntcu.edu.tw>
2022-03-19 17:54:48 +0800

    pcalife0318
    pcalife v1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <VoteComponent />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import VoteComponent from "./components/VoteComponent.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    VoteComponent,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/VoteComponent.vue
@@ -0,0 +1,520 @@
+<template>
+  <div id="vote">
+    <div class="banner">
+      <img src="../assets/img/banner.svg" width="100%" alt="" />
+      <img
+        class="logoimg"
+        src="../assets/img/pcalifelogo.svg"
+        width="100%"
+        alt=""
+      />
+      <div>
+        <img class="diamond" src="../assets/img/red-diamond.svg" alt="" />
+        <img class="diamond" src="../assets/img/blue-diamond.svg" alt="" />
+        <img class="diamond" src="../assets/img/yellow-diamond.svg" alt="" />
+      </div>
+    </div>
+
+    <div class="title-box">
+      <h1 class="vote-title">投票專區</h1>
+      <p>
+        投票注意事項<br />
+        投票期間：2020 年 11 月 24 日（二） 上午 10:00 ~ 2020 年 12 月 10
+        日（四）中午 12:00。<br />
+        本次投票採用 Facebook 臉書帳號投票，所有投票者皆需經過 Facebook
+        臉書登入認證。<br />
+        活動期間，每一個追夢計畫每人每天皆有一票可以表達支持，請廣邀好友踴躍投票。<br />
+        為了投票的公平性，嚴格禁止灌票行為，若有發現任何單位或個人進行不當投票行為，經查證若為決選團體所為，將取消該團體參賽資格。<br />
+        結果公布：整合評審面試分數（60%）+ 網路投票分數（40%），追夢得主將於
+        2020 年 12 月 10 日（四） 下午 7:00 公布。<br />
+        若您使用 APP（例如：LINE）打開此網站，由於 APP
+        內建瀏覽器問題，可能會造成無法投票，建議您使用手機瀏覽器（例如：Google
+        Chrome 或 Safari）開啟。
+      </p>
+    </div>
+    <div v-if="loading">Loading...</div>
+    <div class="groups-row" v-else>
+      <div class="col-lg-6 col-12" v-for="item in groups" v-bind:key="item.id">
+        <div class="group-item">
+          <div class="group-title">{{ item.teamname }}</div>
+          <div class="group-img">
+            <img :src="item.img[0].url" width="100%" alt="" />
+          </div>
+          <div class="group-bottom">
+            <div class="group-proposal">提案：{{ item.title }}</div>
+            <a class="group-btn" href="javascript:;" @click="popopen(item.id)"
+              >查看提案</a
+            >
+          </div>
+        </div>
+      </div>
+      <div>
+        <img class="diamond2" src="../assets/img/blue-diamond.svg" alt="" />
+        <img class="diamond2" src="../assets/img/yellow-diamond.svg" alt="" />
+      </div>
+    </div>
+    <div class="popup-wrap" v-if="popToggle">
+      <div class="popup-box" v-show="popConfirmStep === 0">
+        <div class="group-title">
+          {{ popdata.teamname }}—{{ popdata.title }}
+        </div>
+        <div class="group-img">
+          <img :src="popdata.img[0].url" width="100%" alt="" />
+        </div>
+        <div class="group-intr">{{ popdata.description }}</div>
+        <!-- 投票時間檢測 -->
+        <a
+          v-show="checkVoteTime === true"
+          class="group-btn"
+          href="javascript:;"
+          @click="popStepChange(1)"
+          >投我一票</a
+        >
+        <a
+          v-show="checkVoteTime === false"
+          class="group-btn"
+          href="javascript:;"
+          @click="popclose"
+          >回到提案總覽</a
+        >
+        <p
+          v-show="checkVoteTime === false"
+          style="margin-top: 10px; color: #ff3445; font-size: 14px"
+        >
+          投票時間為：3/21 9:00 ~ 3/30 19:00
+        </p>
+        <a class="close-btn" href="javascript:;" @click="popclose"></a>
+      </div>
+      <div class="popvote-box step1" v-show="popConfirmStep === 1">
+        <h3>確認投票</h3>
+        <h4>*您的Email</h4>
+        <input type="text" v-model.trim="input.email" />
+        <p>如果您願意參與本次抽獎，請協助填寫姓名、電話</p>
+        <h4>姓名</h4>
+        <input type="text" v-model.trim="input.name" />
+        <h4>手機號碼</h4>
+        <input type="text" v-model.trim="input.phone" />
+        <a class="group-btn" href="javascript:;" @click="submitHandler"
+          >送出投票</a
+        >
+        <p>
+          <span>{{ warningtext }}</span>
+        </p>
+        <a class="close-btn" href="javascript:;" @click="popclose"></a>
+      </div>
+      <div class="popvote-box step2" v-show="popConfirmStep === 2">
+        <h3>投票完成</h3>
+        <h4>感謝您的投票！</h4>
+        <a class="group-btn" href="javascript:;" @click="popclose"
+          >回到提案總覽</a
+        >
+        <br /><br />
+        <a class="group-btn" href="javascript:;" @click="popclose"
+          >保誠創新智造所</a
+        >
+      </div>
+    </div>
+    <div v-else />
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "VoteComponent",
+  props: {},
+  data() {
+    return {
+      loading: false,
+      groups: [],
+      input: {
+        email: "",
+        name: "",
+        phone: "",
+        workid: "",
+      },
+      popToggle: false,
+      popConfirmStep: 0,
+      workid: "",
+      warningtext: "",
+      checkVoteTime: false,
+      checkEmailnotExis: null,
+      // nowTime: null,
+    };
+  },
+  computed: {
+    popdata() {
+      return this.groups.find((item) => item.id === this.workid);
+    },
+    // voteTimeCheck(){
+    //   let now = new Date();
+    //   console.log(now);
+    //   return this.voteTime = now;
+    // },
+  },
+  mounted() {
+    this.loading = true;
+    axios
+      .get("https://computer.bkhole.app/artworklists")
+      .then((response) => {
+        this.groups = response.data;
+        this.loading = false;
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  },
+  methods: {
+    submitHandler() {
+      //成功送出 checkEmailnotExis = 1 -> step2
+      //Email錯誤-> step1
+      //Email沒有填寫-> step1
+      //已填寫過 checkEmailnotExis = 2 -> step1
+      axios
+        .get("http://localhost:8888/content?email=" + this.input.email)
+        .then((response) => {
+          if (response.data.length > 0) {
+            console.log("response.data.length:" + response.data.length);
+            this.checkEmailnotExis = 2;
+          } else {
+            console.log("response.data.length:" + response.data.length);
+            this.checkEmailnotExis = 1;
+          }
+          this.checkEmail();
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+    checkEmail() {
+      //檢查Email格式
+      let emailRegxp = /^[^\s]+@[^\s]+\.[^\s]{2,3}$/;
+      if (!this.input.email || emailRegxp.test(this.input.email) == false) {
+        //空值或錯誤
+        this.warningtext = "＊email格式有誤，請重新填寫";
+        console.log("Email沒有提交");
+        this.popStepChange(1);
+      } else if (this.input.email && this.checkEmailnotExis === 1) {
+        //檢查Email沒有重複
+        axios
+          .post("http://localhost:8888/content", this.input)
+          .then((response) => {
+            console.log(response.data);
+            this.cancelHandler();
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+        this.popStepChange(2);
+      } else if (this.input.email && this.checkEmailnotExis !== 1) {
+        //檢查Email有重複
+        this.warningtext = "＊您已經投過票了，謝謝參與！";
+        console.log("Email存在");
+        this.popStepChange(1);
+      }
+    },
+    cancelHandler() {
+      this.input.email = "";
+      this.input.name = "";
+      this.input.phone = "";
+      this.input.workid = "";
+    },
+    popclose() {
+      this.popStepChange(0);
+      this.workid = "";
+      this.warningtext = "";
+      (this.checkEmailnotExis = null), this.cancelHandler();
+    },
+    popopen(workid) {
+      this.timeCheck();
+      this.popToggle = true;
+      this.workid = workid;
+      this.input.workid = workid;
+    },
+    popStepChange(step) {
+      if (step === 0) {
+        this.popToggle = false;
+        this.popConfirmStep = step;
+      } else if (step === 1) {
+        this.popConfirmStep = step;
+      } else if (step === 2) {
+        this.popConfirmStep = step;
+      }
+    },
+    timeCheck() {
+      let voteStart = new Date(2022, 3, 21, 9, 0, 0, 0);
+      let voteEnd = new Date(2022, 3, 30, 19, 0, 0, 0);
+      let now = new Date(2022, 3, 22);
+      // console.log(voteStart+"\n"+voteEnd+"\n"+now);
+      if (now < voteStart) {
+        this.checkVoteTime = false;
+        console.log("還不能投票");
+      } else if (now > voteEnd) {
+        this.checkVoteTime = false;
+        console.log("投票已結束");
+      } else {
+        this.checkVoteTime = true;
+        console.log("投票中");
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+@import "../assets/css/popup.css";
+#vote {
+  position: relative;
+  padding: 50px 207px 150px 207px;
+  overflow-x: hidden;
+}
+.banner {
+  position: relative;
+}
+.logoimg {
+  position: absolute;
+  top: 60%;
+  left: 50%;
+  transform: translate(-50%, -50%) scale(0.65);
+}
+.diamond:nth-child(1) {
+  position: absolute;
+  width: 30%;
+  bottom: -9%;
+  left: -10%;
+}
+.diamond:nth-child(2) {
+  position: absolute;
+  width: 10%;
+  top: 10%;
+  right: -7%;
+}
+.diamond:nth-child(3) {
+  position: absolute;
+  width: 15%;
+  bottom: -30%;
+  right: -15%;
+}
+
+.diamond2:nth-child(1) {
+  position: absolute;
+  width: 8%;
+  top: 40%;
+  right: -10%;
+}
+.diamond2:nth-child(2) {
+  position: absolute;
+  width: 13%;
+  bottom: 10%;
+  right: -15%;
+}
+
+.title-box {
+  text-align: center;
+}
+
+.title-box > .vote-title {
+  display: inline-block;
+  position: relative;
+  font-family: "Quicksand";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 72px;
+  line-height: 90px;
+  letter-spacing: 0.135em;
+  background: linear-gradient(143.9deg, #ed1b2e 0%, #444444 100%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-clip: text;
+  /* text-fill-color: transparent; */
+  margin: 0;
+}
+.title-box > p {
+  font-family: "Quicksand";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 22px;
+  text-align: center;
+  letter-spacing: 0.135em;
+  color: #000000;
+}
+
+.vote-title::before {
+  content: "";
+  position: absolute;
+  width: 20vw;
+  height: 2px;
+  background: #000000;
+  top: 50%;
+  left: -21vw;
+}
+.vote-title::after {
+  content: "";
+  position: absolute;
+  width: 21vw;
+  height: 2px;
+  background: #000000;
+  top: 50%;
+  right: -21vw;
+}
+.groups-row {
+  display: flex;
+  flex-wrap: wrap;
+  margin-right: -15px;
+  margin-left: -15px;
+  position: relative;
+}
+
+.col-lg-6,
+.col-12 {
+  padding: 20px 15px;
+}
+
+.group-item {
+  margin: 0 auto;
+  border: 2px solid #000000;
+  border-radius: 60px;
+  padding: 32px 39px;
+  transition: 0.3s;
+}
+.group-item:hover {
+  box-shadow: 3px 3px 12px 2px #efe6e9;
+}
+
+.group-title {
+  position: relative;
+  font-family: "Quicksand";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 30px;
+  letter-spacing: 0.135em;
+  padding: 0 18px;
+  margin-bottom: 20px;
+  text-align: left;
+}
+
+.group-title::before {
+  content: "";
+  position: absolute;
+  background: #ff3445;
+  display: inline-block;
+  height: 32px;
+  width: 6px;
+  top: 0;
+  left: 0;
+}
+
+.group-img {
+  margin: 0 auto 23px auto;
+  width: 100%;
+}
+
+.group-bottom {
+  display: flex;
+  justify-content: space-between;
+}
+
+.group-proposal {
+  display: inline-block;
+  max-width: 100%;
+  font-family: "Quicksand";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 18px;
+  line-height: 22px;
+  letter-spacing: 0.135em;
+  margin: 0 10px 0 0;
+}
+
+.group-btn {
+  display: inline-block;
+  background: #ff3445;
+  border-radius: 15px;
+  font-family: "Quicksand";
+  font-weight: 700;
+  font-size: 18px;
+  line-height: 25px;
+  text-decoration: none;
+  text-align: center;
+  letter-spacing: 0.135em;
+  color: #ffffff;
+  padding: 18px 20px;
+  margin: 0;
+  cursor: pointer;
+  min-width: 140px;
+  transition: 0.3s;
+}
+.group-btn:hover {
+  background: #df2f3e;
+}
+
+.group-intr {
+  font-family: "Quicksand";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 36px;
+  letter-spacing: 0.2em;
+  color: #68737a;
+  text-align: justify;
+  margin-bottom: 20px;
+}
+
+/* desktops */
+@media (max-width: 1200px) {
+  #vote {
+    padding: 0 120px;
+  }
+}
+@media (max-width: 992px) {
+  #vote {
+    padding: 0 140px;
+  }
+  .title-box > .vote-title {
+    font-size: 55px;
+    line-height: 90px;
+  }
+}
+
+/* tablets */
+@media (max-width: 768px) {
+  #vote {
+    padding: 0 100px;
+  }
+}
+
+/* phones */
+@media (max-width: 576px) {
+  #vote {
+    padding: 0 30px;
+  }
+  .title-box > .vote-title {
+    font-size: 32px;
+    line-height: 70px;
+  }
+  .group-bottom {
+    display: block;
+    text-align: center;
+  }
+  .group-proposal {
+    display: block;
+    max-width: 100%;
+    margin: 0 0 15px 0;
+  }
+  .group-btn {
+    font-size: 16px;
+  }
+  .group-intr {
+    font-size: 17px;
+    line-height: 26px;
+    letter-spacing: 0.1em;
+    color: #68737a;
+    text-align: justify;
+    margin-bottom: 20px;
+  }
+}
+</style>
